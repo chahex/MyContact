d0f8af60fa63b54e04f59bb2794f30dff78f69f4
Xinkai He <cha.hex@gmail.com>
2012-05-28 17:30:07 -0400

    Added the first entity.

---
+++ MyContact/Contact.h
@@ -0,0 +1,13 @@
+//
+//  Contact.h
+//  MyContact
+//
+//  Created by Xinkai HE on 5/28/12.
+//  Copyright (c) 2012 Carnegie Mellon University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Contact : NSObject
+
+@end
---
+++ MyContact/Contact.m
@@ -0,0 +1,13 @@
+//
+//  Contact.m
+//  MyContact
+//
+//  Created by Xinkai HE on 5/28/12.
+//  Copyright (c) 2012 Carnegie Mellon University. All rights reserved.
+//
+
+#import "Contact.h"
+
+@implementation Contact
+
+@end
