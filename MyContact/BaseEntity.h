c9374b578c4ef0cc0b37d808a32ffa41949e5090
Xinkai He <cha.hex@gmail.com>
2012-06-03 10:53:02 -0400

    BE

---
+++ MyContact/BaseEntity.h
@@ -0,0 +1,15 @@
+//
+//  BaseEntity.h
+//  MyContact
+//
+//  Created by  on 6/1/12.
+//  Copyright (c) 2012 Carnegie Mellon University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BaseEntity : NSObject
+
+-(id) init;
+
+@end
